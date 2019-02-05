bb7122d1b78727e5afceb432a4e664e610aad69f
Dimitri DeFigueiredo <defigueiredo@ucdavis.edu>
2019-02-05 16:16:40 -0200

    WIP: Make producer/executor types explicit
    This is just a first step. Save point.

--- app/Main.hs
+++ app/Main.hs
@@ -2,7 +2,7 @@
 
 module Main where
 
--- import Control.Concurrent           (threadDelay)
+import Control.Concurrent           (threadDelay)
 import Control.Concurrent.Async
 
 import Reactive.Banana
@@ -10,12 +10,7 @@ import Reactive.Banana.Frameworks.Extended
 
 import Pipes.Concurrent
 
-import System.IO                (hPutStr, hPutStrLn, stderr) -- Remove me!
-
--- import Market.Types
--- import GDAXProducer
--- import GDAXAdapter
--- import GDAXExecutor
+import System.IO                    (hPutStr, hPutStrLn, stderr) -- Remove me!
 
 import Trading.Framework
 import Trading.Strategy
@@ -31,25 +26,35 @@ This program uses multiple threads with an event network using our "push-pull" m
 3. Execution threads that process the Actions put in their respective action queues
 -}
 
--- mirrorStrategy
---     :: forall m p v q c. (MonadMoment m, Coin p, Coin v)
---     => Vol v -> Event (TradingEv p v q c) -> Event (TradingEv p v q c) 
--- -> m (Event (Maybe (StrategyAdvice (Action p v)), Maybe (StrategyAdvice (Action p v)))) 
-
 showBook 
     :: forall m p v q c. (MonadMoment m, Coin p, Coin v)
     => Event (TradingEv p v q c) 
     -> m (Event (Maybe (StrategyAdvice (Action p v))))
 showBook _ = return never
 
+coinbeneInitializer :: IO ()
+coinbeneInitializer = return ()
 
+producer
+    :: forall p v q c config state. (Coin p, Coin v)
+    => config -> state -> (TradingEv p v q c -> IO ()) 
+    -> IO ()
+producer config state handler = return ()
 
-getCoinbeneConfig = return ()
+executor 
+    :: forall p v config state action. (Coin p, Coin v) 
+    => config -> state -> Action p v
+    -> IO ()
+executor _config _state = print
+
+terminator :: config -> state -> IO ()
+terminator _ _ = hPutStrLn stderr "\nExecutor exiting!"
 
 main :: IO ()
 main = do
 
-    coinbeneConfig <- getCoinbeneConfig
+    coinbeneConfig <- coinbeneInitializer -- (should also return initial state)
+    let sharedState = undefined
 
     putStrLn "--------------------------- Starting --------------------------------"
     putStrLn "Type <ENTER> to quit"
@@ -57,57 +62,38 @@ main = do
 
     -- event dispatch handlers
     (handlersUSDTBTC, fireUSDTBTC) <- newHandlerSet
-    -- (handlersUSDLTC, fireUSDLTC) <- newHandlerSet
-    -- (handlersBTCLTC, fireBTCLTC) <- newHandlerSet
+
     -- execution FIFO queues
     (outputUSDTBTC, inputUSDTBTC, stopExecutorUSDTBTC) <- spawn' unbounded
-    -- (outputUSDLTC, inputUSDLTC, stopExecutorUSDLTC) <- spawn' unbounded
-    -- (outputBTCLTC, inputBTCLTC, stopExecutorBTCLTC) <- spawn' unbounded
 
     network <- compile $ do
-        esUSDTBTC  <- fromHandlerSet handlersUSDTBTC
-    --     esUSDLTC  <- fromHandlerSet handlersUSDLTC
-    --     esBTCLTC  <- fromHandlerSet handlersBTCLTC
-
-        esAdvice <- showBook esUSDTBTC
-    --     (eUSDBTC, eUSDLTC, eBTCLTC) <- showAllBooks
-    --         (esUSDBTC :: Event (GDAXTradingE USD BTC))
-    --         (esUSDLTC :: Event (GDAXTradingE USD LTC))
-    --         (esBTCLTC :: Event (GDAXTradingE BTC LTC))
-
-        reactimate $ fmap (maybe (pure ()) (logAndQueue outputUSDTBTC)) (esAdvice :: Event (Maybe (StrategyAdvice (Action USD BTC))))
-        -- reactimate $ fmap (logAndQueue outputUSDLTC) eUSDLTC
-
-    --     reactimate $ fmap (logAndQueue outputBTCLTC) eBTCLTC
+        esUSDTBTC <- fromHandlerSet handlersUSDTBTC
+        esAdvice  <- showBook esUSDTBTC
+        reactimate $ 
+            fmap (maybe (pure ()) (logAndQueue outputUSDTBTC)) 
+            (esAdvice :: Event (Maybe (StrategyAdvice (Action USD BTC))))
 
     -- start sensory threads
-
-    -- fromMarketStream gdaxConfig (fireUSDBTC . TB) (fireUSDBTC . TP) (fireUSDBTC . TC) (fireUSDBTC . TF)
-    -- fromMarketStream gdaxConfig (fireUSDLTC . TB) (fireUSDLTC . TP) (fireUSDLTC . TC) (fireUSDLTC . TF)
-    -- fromMarketStream gdaxConfig (fireBTCLTC . TB) (fireBTCLTC . TP) (fireBTCLTC . TC) (fireBTCLTC . TF)
+    prodUSDTBTC <- async $ producer coinbeneConfig sharedState fireUSDTBTC
+    link prodUSDTBTC
 
     -- start execution threads
-    execUSDTBTC <- async $ runExecutor inputUSDTBTC print (hPutStrLn stderr "\nExecutor exiting!") 
+    execUSDTBTC <- async $ runExecutor 
+                    inputUSDTBTC 
+                    (executor   coinbeneConfig sharedState) 
+                    (terminator coinbeneConfig sharedState) 
     link execUSDTBTC
-
-    -- execUSDLTC <- async $ runExecutor (doAtGDAX gdaxConfig) inputUSDLTC
-    -- link execUSDLTC
-
-    -- execBTCLTC <- async $ runExecutor (doAtGDAX gdaxConfig) inputBTCLTC
-    -- link execBTCLTC
 
     -- run until users presses <ENTER> key
     activate network
     keyboardWait
 
-    -- -- Shutdown
-    -- -- 30 seconds timeout for executors to finish their job before terminating
-    -- atomically stopExecutorUSDBTC
-    -- atomically stopExecutorUSDLTC
-    -- atomically stopExecutorBTCLTC
-    -- race_
-    --     (threadDelay (30 * 1000000))
-    --     (mapConcurrently_ wait [execUSDBTC, execUSDLTC, execBTCLTC])
+    -- Shutdown
+    -- 30 seconds timeout for executors to finish their job before terminating
+    atomically stopExecutorUSDTBTC
+    race_
+        (threadDelay (30 * 1000000))
+        (mapConcurrently_ wait [execUSDTBTC])
 
 
 --------------------------------------------------------------------------------
