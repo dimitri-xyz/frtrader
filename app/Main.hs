f74cd187cb091baf73311913c95d38d3800bd615
Dimitri DeFigueiredo <defigueiredo@ucdavis.edu>
2019-05-01 16:18:21 -0700

    Add USDT straight up buying or selling strategy
    This is just super dumb. The strategy keeps the
    target open on the orderbook at all times.

--- app/Main.hs
+++ app/Main.hs
@@ -2,11 +2,19 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 
 #ifdef ETHEREUM
-#define MACRO_CURRENCY ETH
-#define MACRO_BUCKETSIZE 1
+#define D_CURRENCY ETH
+#define D_BUCKETSIZE 1
+#define D_USAGE_MSG     "Usage: frt-eth pollRate mirrorSide maxExposure slipVol placeVol USDT-SellPrice USDT-BuyPrice"
+
+#elif TETHER
+#define D_CURRENCY      USD
+#define D_BUCKETSIZE    undefined
+#define D_USAGE_MSG     "Usage: frt-usdt pollRate mirrorSide maxExposure slipVol(ignored) placeVol USDT-SellPrice USDT-BuyPrice"
+
 #else
-#define MACRO_CURRENCY BTC
-#define MACRO_BUCKETSIZE 20
+#define D_CURRENCY      BTC
+#define D_BUCKETSIZE    20
+#define D_USAGE_MSG     "Usage: frt pollRate mirrorSide maxExposure slipVol placeVol USDT-SellPrice USDT-BuyPrice"
 #endif
 
 module Main where
@@ -29,13 +37,12 @@ import Reactive.Banana.Frameworks.Extended
 import Pipes.Concurrent
 
 import Trading.Framework
-import Trading.Strategy             (mirrorStrategy2, defineTarget, AskSide(..), BidSide(..), mirrorStrategy3)
+import Trading.Strategy             (mirrorStrategy2, defineTarget, AskSide(..), BidSide(..), mirrorStrategy3, keepOpenStrategy, fixedTarget)
 import Market.Interface
 import Reactive.Banana.Combinators  (never, filterE) -- FIX ME! remove me!
 import Market.Coins
 
 import Coinbene.Connector           (Coinbene(..), API_ID(..), API_KEY(..), Verbosity(..), getCoinbeneConfig, coinbeneInit)
--- import Coinbene                     (Coinbene(..), API_ID(..), API_KEY(..), Verbosity(..)) -- FIX ME! Remove me.
 
 {-
 This program uses multiple threads with an event network using our "push-pull" model.
@@ -55,15 +62,15 @@ main = do
     args <- getArgs
 
     if length args /= 7 then
-        error "Usage: frt pollRate mirrorSide maxExposure slipVol placeVol USDT-SellPrice USDT-BuyPrice"
+        error D_USAGE_MSG
     else
         return ()
 
     let pollingInterval :: Double =                 ((read $ args !! 0) :: Double )
         mirrorSide      :: String =                          args !! 1
-        maxExposure :: Vol MACRO_CURRENCY = Vol   $ realToFrac ((read $ args !! 2) :: Double )
-        slipVol     :: Vol MACRO_CURRENCY = Vol   $ realToFrac ((read $ args !! 3) :: Double )
-        placeVol    :: Vol MACRO_CURRENCY = Vol   $ realToFrac ((read $ args !! 4) :: Double )
+        maxExposure :: Vol D_CURRENCY = Vol   $ realToFrac ((read $ args !! 2) :: Double )
+        slipVol     :: Vol D_CURRENCY = Vol   $ realToFrac ((read $ args !! 3) :: Double )
+        placeVol    :: Vol D_CURRENCY = Vol   $ realToFrac ((read $ args !! 4) :: Double )
         xSellRate :: Price BRL = Price $ realToFrac ((read $ args !! 5) :: Double )
         xBuyRate  :: Price BRL = Price $ realToFrac ((read $ args !! 6) :: Double )
 
@@ -83,28 +90,37 @@ main = do
 
     -- event dispatch handlers
     (ctrlHandlers, fireControl) <- newHandlerSet
+#ifndef TETHER
     (handlers1, fire1) <- newHandlerSet
+#endif
     (handlers2, fire2) <- newHandlerSet
 
     -- execution FIFO queues
     (ctrlOutput, ctrlInput, stopCtrlExecutor) <- spawn' unbounded
+#ifndef TETHER
     (output1   , input1   , stopExecutor1)    <- spawn' unbounded
+#endif
     (output2   , input2   , stopExecutor2)    <- spawn' unbounded
 
     -- Initialize Connectors
+#ifndef TETHER
     (producer1, executor1, terminator1) <- coinbeneInit (truncate $ pollingInterval * 1000000) Verbose coinbeneConfig (Proxy :: Proxy IO) fire1
+#endif
     (producer2, executor2, terminator2) <- coinbeneInit (truncate $ pollingInterval * 1000000) Verbose coinbeneConfig (Proxy :: Proxy IO) fire2
 
 
     -- Build and start the strategy
     network <- compile $ do
         ctrlEs <- fromHandlerSet ctrlHandlers
+#ifndef TETHER
         es1    <- fromHandlerSet handlers1
+#endif
         es2    <- fromHandlerSet handlers2
 
+#ifndef TETHER
         triple <- case mirrorSide of
-                "ASKS" -> mirrorStrategy3 xSellRate xBuyRate maxExposure (defineTarget MACRO_BUCKETSIZE asks slipVol placeVol) AskSide ctrlEs es1 es2
-                "BIDS" -> mirrorStrategy3 xSellRate xBuyRate maxExposure (defineTarget MACRO_BUCKETSIZE bids slipVol placeVol) BidSide ctrlEs es1 es2
+                "ASKS" -> mirrorStrategy3 xSellRate xBuyRate maxExposure (defineTarget D_BUCKETSIZE asks slipVol placeVol) AskSide ctrlEs es1 es2
+                "BIDS" -> mirrorStrategy3 xSellRate xBuyRate maxExposure (defineTarget D_BUCKETSIZE bids slipVol placeVol) BidSide ctrlEs es1 es2
                 _      -> error $ "Argument 'mirrorSide' must be either ASKS or BIDS (in all caps)."
 
         let ctrlAs' = fst3 <$> triple
@@ -114,31 +130,50 @@ main = do
             esAdv1 = fromJust <$> filterE isJust esAdv1'
             esAdv2 = fromJust <$> filterE isJust esAdv2'
 
+#else
+        pair <- case mirrorSide of
+                "ASKS" -> keepOpenStrategy maxExposure (fixedTarget xSellRate placeVol) AskSide ctrlEs es2
+                "BIDS" -> keepOpenStrategy maxExposure (fixedTarget xBuyRate  placeVol) BidSide ctrlEs es2
+                _      -> error $ "Argument 'mirrorSide' must be either ASKS or BIDS (in all caps)."
+
+        let ctrlAs' = fst <$> pair
+            esAdv2' = snd <$> pair
+            ctrlAs = fromJust <$> filterE isJust ctrlAs'
+            esAdv2 = fromJust <$> filterE isJust esAdv2'
+
+#endif
+
         reactimate $
             fmap (logAndQueueControl ctrlOutput)
             (ctrlAs :: Event (ControlAction))
 
+#ifndef TETHER
         reactimate $
             fmap (logAndQueueAdvice output1)
-            (esAdv1 :: Event (StrategyAdvice (Action USD MACRO_CURRENCY)))
+            (esAdv1 :: Event (StrategyAdvice (Action USD D_CURRENCY)))
+#endif
 
         reactimate $
             fmap (logAndQueueAdvice output2)
-            (esAdv2 :: Event (StrategyAdvice (Action BRL MACRO_CURRENCY)))
+            (esAdv2 :: Event (StrategyAdvice (Action BRL D_CURRENCY)))
 
     activate network
 
     -- start execution threads
     ctrlExecThread <- async $ runExecutor ctrlInput (ctrlExecutor stopCtrlExecutor) ctrlFinalizer
     link ctrlExecThread
+#ifndef TETHER
     exec1          <- async $ runExecutor input1     executor1 terminator1
     link exec1
+#endif
     exec2          <- async $ runExecutor input2     executor2 terminator2
     link exec2
 
     -- start producer threads
+#ifndef TETHER
     prod1 <- async producer1
     link prod1
+#endif
     prod2 <- async producer2
     link prod2
 
@@ -159,11 +194,17 @@ main = do
 
     -- The strategy either already shutdown or should killed at this point
     -- give 30 seconds (if needed) for executors to finish their job before terminating
+#ifndef TETHER
     atomically stopExecutor1
+#endif
     atomically stopExecutor2
     race_
         (threadDelay (30 * 1000000))
-        (mapConcurrently_ wait [exec1, exec2])
+        (mapConcurrently_ wait [
+#ifndef TETHER
+            exec1,
+#endif
+            exec2])
 
     case eExitCode of
         Left  _ -> exitWith (ExitFailure 89) -- let's just say 89 is strategy timeout
